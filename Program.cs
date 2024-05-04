3e17fcb00068b6e7df7087ae910ceae5a9e05dee
BaoHG <baohuynh291@gmail.com>
2024-05-04 14:29:17 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class PrimeNumberChecker
+{
+    static void Main()
+    {
+        Console.Write("Nhập vào một số để kiểm tra: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine($"{number} là số nguyên tố.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} không phải là số nguyên tố.");
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+
+        int boundary = (int)Math.Sqrt(number);
+        for (int i = 2; i <= boundary; i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
